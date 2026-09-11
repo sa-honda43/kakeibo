993915143c75b21b84e440ef9987fdd243394c96
sa-honda43 <s.honda9543@gmail.com>
2026-09-11 11:52:52 +0900

    Initial commit

---
+++ kakeibo-backend/src/main/java/com/example/kakeibo_backend/KakeiboBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.kakeibo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakeiboBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakeiboBackendApplication.class, args);
+	}
+
+}
